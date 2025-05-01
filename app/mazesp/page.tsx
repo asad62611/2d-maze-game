91d82771991c37824dca2b9363ea695e63cfdcde
asad62611 <162749350+asad62611@users.noreply.github.com>
2025-05-01 14:37:48 +0200

    Update page.tsx

--- app/mazesp/page.tsx
+++ app/mazesp/page.tsx
@@ -12,7 +12,7 @@ export default function Mazesppage() {
 
   return (
     <main>
-      <ReturnButton />
+      <ReturnButton socket={socketRef.current} />
       <Mazesp difficulty={difficulty ?? "easy"} />
     </main>
   );
