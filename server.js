1672f62106bb428dd2f36728c2d01ccd5b1c8dea
asad62611 <162749350+asad62611@users.noreply.github.com>
2025-05-01 15:23:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-  origin: "https://twod-maze-game.onrender.com",
+  origin: "https://twod-maze-frontend.onrender.com",
   methods: ["GET", "POST"]
 },
 });
